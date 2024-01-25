d4f38975c582769e9aa572845cc26e57c0d1ea45
Razeen <razeenbaig10@gmail.com>
2024-01-25 14:43:59 +0500

    changes made and frontend starting.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 function App() {
-
   return (
     <div>
-        Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
